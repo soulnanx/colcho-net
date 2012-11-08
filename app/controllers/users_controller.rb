84edf7d6ab45eae0df9201d5d5e0e2d017acd066
Renan <renan.dossantos@callsp.inf.br>
2012-11-08 10:31:15 -0200

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(params[:user])
+		if @user.save
+			redirect_to @user,
+						:notice => 'Cadastro criado com sucesso!'
+		else
+			render :new
+		end
+	end
+
+	def show 
+		@user = User.find(params[:id])
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+
+		if @user.update_attributes(params[:id])
+			redirect_to @user, :notice => 'Cadastro atualizado com sucesso!'
+		else
+			render :update
+		end		
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Colchonet::Application.config.secret_token = 'fe83eea02ff88dc7fb2f0957ed5ef88c9d21c096b519c142f68ec470a1bf02e51dfa34f557db791eb3d6ce2ab917d2dbbe91d5416ba69cc8cc213e763283e92c'
